e3690bb23885c7ad35bb32bd4cb1f6a9765fea50
Sabekun-Fariha <144788946+Sabekun-Fariha@users.noreply.github.com>
2025-05-13 23:47:15 +0600

    max_heap.cpp

--- Code/C++/Heap/max_heap.cpp
+++ Code/C++/Heap/max_heap.cpp
@@ -1,13 +1,10 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 void swap(int *a, int *b){
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-
 void maxHeapify(vector<int> &heap, int size, int i){
     int largest = i;
     int leftChild = 2*i + 1;
@@ -25,24 +22,21 @@ void maxHeapify(vector<int> &heap, int size, int i){
         maxHeapify(heap, size, largest);
     }
 }
-
 void buildMaxHeap(vector<int> &heap, int size){
     for(int i = (size/2)-1; i>=0; i--){
         maxHeapify(heap, size, i);
     }
 }
-
 void printHeap(vector<int> &heap){
     for(int i: heap){
         cout << i << " ";
     }
     cout<<endl;
 }
-
 void insertNode(vector<int> &heap, int value){
     heap.push_back(value);             // Add at the end
     int index = heap.size() - 1;
-
+   
     // Heapify upward
     while(index > 0){
         int parent = (index - 1) / 2;
@@ -66,12 +60,11 @@ void deleteNode(vector<int> &heap, int value){
             break;
         }
     }
-
     if(index == -1){
         cout << "Value not found in heap." << endl;
         return;
     }
-
+    
     // Replace with last element and remove last
     heap[index] = heap.back();
     heap.pop_back();
@@ -90,7 +83,6 @@ vector<int> heapSortDecreasing(vector<int> arr){
         arr.pop_back();
         maxHeapify(arr, arr.size(), 0);
     }
-
     return sorted;
 }
 
@@ -98,21 +90,17 @@ int main()
 {
     vector<int> heap = {3, 9, 2, 1, 4, 5};
     int size = heap.size();
-
     cout << "Original Array:" << endl;
     printHeap(heap);
-
     buildMaxHeap(heap, size);
-
     cout << "Max Heap after build:" << endl;
     printHeap(heap);
-
+    
     // Insert a new node
     int newValue = 12;
     cout << "\nInserting value: " << newValue << endl;
     insertNode(heap, newValue);
     printHeap(heap);
-
     /*
     // User Input
     cout<<"Enter a number: ";
@@ -120,16 +108,13 @@ int main()
     cin>>num;
     insertNode(heap, num);
     */
-
     // Delete a node
     int deleteValue = 12;
     cout << "\nDeleting value: " << deleteValue << endl;
     deleteNode(heap, deleteValue);
     printHeap(heap);
 
-
     vector<int> arr = {5, 3, 8, 1, 2, 9};
-
     cout << "Before Sorting: ";
     for(int i : arr) cout << i << " ";
     cout << endl;
@@ -138,8 +123,6 @@ int main()
     cout << "Decreasing Order: ";
     for(int i : decSorted) cout << i << " ";
     cout << endl;
-
-
+    
     return 0;
-
 }
