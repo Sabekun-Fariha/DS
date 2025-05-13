a1a5a66248355e52c6cd9621bb67842e673f875d
Sabekun-Fariha <144788946+Sabekun-Fariha@users.noreply.github.com>
2025-05-14 02:28:35 +0600

    DFS.cpp

--- Code/C++/Graph/DFS.cpp
+++ Code/C++/Graph/DFS.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Graph{
     public:
     void dfs(int start, vector<int>adj[], int visited[], vector<int>&result){
         visited[start] = 1;
         result.push_back(start);
-        
         // Traverse all its neighbours
         for(auto it:adj[start]){
             if(!visited[it]){
@@ -14,8 +12,7 @@ class Graph{
             }
         }
     }
-
-    vector<int> dfsOfGraph(int v, vector<int>adj[]){
+ vector<int> dfsOfGraph(int v, vector<int>adj[]){
         int visited[v] = {0};
         int start = 0;
         vector<int> result;
@@ -23,27 +20,22 @@ class Graph{
         return result;
     }
 };
-
 int main()
 {
     int v = 6;
     vector<int> adj[v];
-
     adj[0] = {1, 3};
     adj[1] = {0, 2, 3};
     adj[2] = {1, 3}; 
     adj[3] = {0, 1, 2, 4, 5};
     adj[4] = {3};
     adj[5] = {3}; 
-
     Graph g;
     vector<int> result = g.dfsOfGraph(v, adj);
-
     cout<<"DFS Traversal: ";
     for(int node: result){
         cout<< node <<" ";
     }
     cout<<endl;
-
     return 0;
 }
